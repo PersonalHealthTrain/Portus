28a70a9a342c2e52a26494e4db78dd7034491288
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-08-23 15:48:25 +0200

    Train: Add Route for stations
    Signed-off-by: Lukas Zimmermann <luk.zim91@gmail.com>

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 class StationsController < ApplicationController
-  before_action :set_repository, only: %i[show toggle_star]
+  #before_action :set_repository, only: %i[show toggle_star]
 
   # GET /stations
   # GET /stations.json
+
   def index
     @repositories = policy_scope(Repository)
     @team_repositories = Repository
-                         .joins(namespace: { team: :users })
-                         .where("users.id = :user_id", user_id: current_user.id)
+                           .joins(namespace: { team: :users })
+                           .where("users.id = :user_id", user_id: current_user.id)
     @other_repositories = @repositories - @team_repositories
     @team_repositories_serialized = API::Entities::Repositories.represent(
       @team_repositories,
---
+++ config/routes/stations.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+
+resources :stations, only: %i[index]
+
+# Used as a reference for writing this f
+
+# resources :repositories, only: %i[index show destroy] do
+#   post :toggle_star, on: :member
+#   resources :comments, only: %i[create destroy]
+# end
+#
+# resources :tags, only: %i[show destroy]
