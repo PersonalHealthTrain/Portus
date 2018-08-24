371ca3484ac4ca616451f40993e7c7b6be28a875
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-08-24 15:40:27 +0200

    Fix: Missing include in main.js
    Signed-off-by: lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>

--- app/assets/javascripts/modules/stations/index.js
+++ app/assets/javascripts/modules/stations/index.js
@@ -3,9 +3,9 @@ import Vue from 'vue';
 import StationsIndexPage from './pages/index';
 
 $(() => {
-  if (!$('body[data-controller="stations"]').length) {
-    return;
-  }
+  // if (!$('body[data-controller="stations"]').length) {
+  //   return;
+  // }
 
   // eslint-disable-next-line no-new
   new Vue({
