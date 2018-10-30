266205a09a14dbb04db18ee4327d176e596aa600
Lukas Zimmermann <luk.zim91@gmail.com>
2018-10-30 14:39:30 +0100

    ui: Add toggle link for repositories index page
    Signed-off-by: Lukas Zimmermann <luk.zim91@gmail.com>

--- app/assets/javascripts/modules/repositories/components/panel.vue
+++ app/assets/javascripts/modules/repositories/components/panel.vue
@@ -4,9 +4,12 @@
       {{ title }}
     </h5>
 
-    <div slot="heading-right">
-      <slot name="heading-right"></slot>
-    </div>
+    <toggle-link
+      slot="heading-right"
+      text="Create new Repository"
+      :state="state"
+      state-key="newFormVisible"
+      class="toggle-link-new-repository"></toggle-link>
 
     <div slot="body">
       <repositories-table :repositories="repositories" :repositories-path="repositoriesPath" :namespaces-path="namespacesPath" :sortable="true" sort-by="name" :show-namespaces="showNamespaces"></repositories-table>
@@ -37,7 +40,13 @@
         default: true,
       },
     },
-
+    data() {
+      return {
+        state: {
+          newFormVisible: false,
+        },
+      };
+    },
     components: {
       RepositoriesTable,
     },
