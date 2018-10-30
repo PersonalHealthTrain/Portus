f91fc20e0a234612da83511d394f435188c873fc
Lukas Zimmermann <luk.zim91@gmail.com>
2018-10-30 09:35:51 +0100

    ui: Remove physics from nodes

--- app/assets/javascripts/modules/repositories/components/route/route-panel.vue
+++ app/assets/javascripts/modules/repositories/components/route/route-panel.vue
@@ -17,17 +17,45 @@
     props: {
       tags: [Array],
     },
+
     data() {
       return {
         network: null,
+        previousNumberOfTags: 0,
+        options: {
+          edges: {
+            arrows: {
+              to: {
+                enabled: true,
+                scaleFactor: 1,
+                type: 'arrow',
+              },
+              middle: {
+                enabled: false,
+                scaleFactor: 1,
+                type: 'arrow',
+              },
+              from: {
+                enabled: false,
+                scaleFactor: 1,
+                type: 'arrow',
+              },
+            },
+          },
+          physics: {
+            enabled: false,
+          },
+        },
       };
     },
-
     computed: {
       refresh() {
-        if (this.tags.length === 0 || this.network !== null) {
+        const numberOfTags = this.tags.length;
+        // Number of tags has not changed in this request, so we skip redrawing
+        if (numberOfTags === this.previousNumberOfTags) {
           return '';
         }
+        this.previousNumberOfTags = numberOfTags;
         // Compute the nodes
         const nodes = this.tags.map((x) => {
           const node = x[0];
@@ -38,20 +66,15 @@
           };
         }).sort((a, b) => a.created_at - b.created_at);
         // compute edges
-        const edges = Array.from(new Array(this.tags.length - 1), (_, i) => i)
-          .map((i) => {
-            return {
-              from: nodes[i].id,
-              to: nodes[i + 1].id,
-            };
-          });
-        const options = {};
+        const edges = Array
+          .from(new Array(numberOfTags - 1), (_, i) => i)
+          .map(i => ({ from: nodes[i].id, to: nodes[i + 1].id }));
         this.network = new vis.Network(document.getElementById('mynetwork'), {
           nodes: nodes.map((x) => {
-            return { id: x.id, name: x.name };
+            return { id: x.id, label: x.name };
           }),
           edges: edges,
-        }, options);
+        }, this.options);
         return '';
       },
     },
